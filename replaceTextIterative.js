29b90c058498f22686b59cbdc569016fec863f5e
Victor Chan <victorchan88888888@gmail.com>
2017-06-23 00:24:09 -0700

    Finished replaceTextIterative method

--- replaceTextIterative.js
+++ replaceTextIterative.js
@@ -1,4 +1,4 @@
 var elements = document.getElementsByTagName("*");
 for (var i = 0; i<elements.length; i++) {
-    elements[i].textContent = elements[i].textContent.replace(, "The Number One Public University in the World");
+    elements[i].textContent = elements[i].textContent.replace(/berkeley|uc berkeley| cal /g, "The Number One Public University in the World");
 }
