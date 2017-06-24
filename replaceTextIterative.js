1fd6a9c9d21786cf70e4085bd9bfe278724b957a
Victor Chan <victorchan88888888@gmail.com>
2017-06-24 01:28:22 -0700

    Fixed some problems in replaceTextIterative

--- replaceTextIterative.js
+++ replaceTextIterative.js
@@ -1,4 +1,7 @@
-var elements = document.getElementsByTagName("*");
-for (var i = 0; i<elements.length; i++) {
-    elements[i].textContent = elements[i].textContent.replace(/berkeley|uc berkeley| cal /g, "The Number One Public University in the World");
+function replaceText() {
+    var elements = document.getElementsByTagName("*");
+    for (var i = 0; i<elements.length; i++) {
+        elements[i].innerText = elements[i].innerText.replace(/uc berkeley|ucberkeley|berkeley| cal /gi, "The Number One Public University in the World");
+    }
 }
+window.onload = replaceText();
