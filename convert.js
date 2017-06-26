4ad01a6168a501016448d277f9492c06f93b3299
Victor Chan <victorchan88888888@gmail.com>
2017-06-25 21:01:34 -0700

    Deleted iterative version of replaceText() and updated manifest

--- convert.js
+++
@@ -1,3 +0,0 @@
-function convert(s) {
-    return s.replace(/university of california, berkeley|uc berkeley|ucberkeley|berkeley| cal /gi, "The Number One Public University in the World");
-}
--- replaceTextRecursive.js
+++ replaceTextRecursive.js
@@ -1,3 +1,7 @@
+function convert(s) {
+    return s.replace(/university of california, berkeley|uc berkeley|ucberkeley|berkeley| cal /gi, "The Number One Public University in the World");
+}
+
 function replaceText(element) {
     var elements = element.childNodes;
     for (var i=0; i<elements.length; i++) {
