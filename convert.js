2879a400ecaaa8175fe191f548ff7b7f88c2a038
Victor Chan <victorchan88888888@gmail.com>
2017-06-25 20:32:22 -0700

    Created function to replace text in string

---
+++ convert.js
@@ -0,0 +1,3 @@
+function convert(s) {
+    return s.replace(/university of california, berkeley|uc berkeley|ucberkeley|berkeley| cal /gi, "The Number One Public University in the World");
+}
--- replaceTextRecursive.js
+++ replaceTextRecursive.js
@@ -1,8 +1,8 @@
-function replaceText(element) {
+unction replaceText(element) {
     var elements = element.childNodes;
     for (var i=0; i<elements.length; i++) {
         if (elements[i].nodeType == 3) {
-            elements[i].textContent = elements[i].textContent.replace(/university of california, berkeley|uc berkeley|ucberkeley|berkeley| cal /gi, "The Number One Public University in the World");
+            elements[i].textContent = convert(elements[i].textContent);
         } else {
             replaceText(elements[i]);
         }
