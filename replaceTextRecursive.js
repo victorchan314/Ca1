08de1d4e6930bdd826e73e32dc44d45b13fd100a
Victor Chan <victorchan88888888@gmail.com>
2017-06-25 20:22:31 -0700

    Converted to recursive replacement

--- replaceTextRecursive.js
+++ replaceTextRecursive.js
@@ -1,4 +1,11 @@
-function replaceTextRecursive(element) {
-    element.textContent 
+function replaceText(element) {
+    var elements = element.childNodes;
+    for (var i=0; i<elements.length; i++) {
+        if (elements[i].nodeType == 3) {
+            elements[i].textContent = elements[i].textContent.replace(/university of california, berkeley/uc berkeley|ucberkeley|berkeley| cal /gi, "The Number One Public University in the World");
+        } else {
+            replaceText(elements[i]);
+        }
+    }
 }
-replaceText(document.body);
+window.onload = replaceText(document.body);
